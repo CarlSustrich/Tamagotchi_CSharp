7581ea6d886198640ca5b22e08f23496f1afe1ca
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-03-01 14:22:31 -0800

    working link to pet ID, WIP to display details

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -9,7 +9,7 @@ namespace Tamagotchi.Models
     public int Id {get;}
     public int Food {get;set;}
     public int Attention {get;set;}
-    public int Sleep {get;set;}
+    public int Sleep {get;set;} 
     private static List<Pet> _instances = new List<Pet> {};
 
     public Pet(string name)
@@ -19,7 +19,7 @@ namespace Tamagotchi.Models
       Attention = 100;
       Sleep = 100;
       _instances.Add(this);
-      Id = _instances.Count;
+      Id = _instances.Count; 
     }
 
     public static List<Pet> GetAll()
@@ -27,6 +27,7 @@ namespace Tamagotchi.Models
       return _instances;
     }
 
+    
     public static Pet Find(int searchId)
     {
       return _instances[searchId-1];
@@ -37,6 +38,18 @@ namespace Tamagotchi.Models
       _instances.Clear();
     }
 
-    public F
+    public void FoodIntake()
+    {
+      if (Food+10 <= 100)
+      {
+        Food += 10;
+      }
+      else
+      {
+        Food = 100;
+      }
+      
+      //return Increment + 10;
+    }
   }
 }
